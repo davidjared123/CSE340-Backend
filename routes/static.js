49a42a0ae394adfb5664325019ce55551802506d
David Perez <davidjared123@gmail.com>
2025-11-04 23:01:58 -0400

    first commit w01

--- routes/static.js
+++ routes/static.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use("/css", express.static(__dirname + "/../public/css"));
+router.use("/js", express.static(__dirname + "/../public/js"));
+router.use("/images", express.static(__dirname + "/../public/images"));
+
+// Home route
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
 module.exports = router;
 
